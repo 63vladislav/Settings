e6933e343cc63155760e30cc4cd5cba05fbda6cb
63vladislav <151637691+63vladislav@users.noreply.github.com>
2024-03-24 13:50:37 +0400

    Initial Commit

---
+++ Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by 111 on 24.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
